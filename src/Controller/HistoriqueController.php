df119ed596634c4d0b980226e05c8c665b51b1a3
Melek-E <86518925+Melek-E@users.noreply.github.com>
2022-06-14 23:13:19 +0000

    Update HistoriqueController.php

--- src/Controller/HistoriqueController.php
+++ src/Controller/HistoriqueController.php
@@ -1,51 +1,45 @@
 <?php
 namespace App\Controller;
-set_time_limit(60);
-use Exception; 
+ 
 use App\Entity\Historique;
+use AppBundle\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 /**
  * @Route("/api", name="api_")
  */
 class HistoriqueController extends AbstractController
-{   
-
- 
- 
-      /**
-     * @Rest\Get("/historiques", name="app_historique_list")
-     * @View
-     */
-    public function listAction(EntityManagerInterface $em)
-    {
-        $historiques = $em->getRepository('App\Entity\HistoriqueDepanneur')->findAll();
-        return $historiques;
-    }
-
-
-    #$articles = $this->getDoctrine()->getRepository('App\Entity\HistoriqueDepanneur')->findAll();
-        #return $articles;
+{ 
     
     /**
+    * @Rest\Get("/historiques", name="app_historiques_list")
+    * @View
+    */
+
+   public function listAction(EntityManagerInterface $em)
+   {
+       $historiques = $em->getRepository('App\Entity\Historique')->findAll();
+       return $historiques;
+   }
+ 
+   
+
+
+    /**
      * @Rest\Post(
-     *    path = "historique",
-     *    name = "historique"
+     *    path = "/historique",
+     *    name = "app_historique_create"
      * )
      * @Rest\View(StatusCode = 201)
      * @ParamConverter("historique", converter="fos_rest.request_body")
@@ -59,18 +53,42 @@ class HistoriqueController extends AbstractController
         
         return $historique;
     }
+
   /**
      * @Get(
      *     path = "/historique/{id}",
-     *     name = "app_historique_show",
+     *     name = "app_historique _show",
      *     requirements = {"id"="\d+"}
      * )
      * @View
      */
     public function showAction(Historique $historique)
     {
-    
         return $historique;
+    }
+ 
+    /**
+     * @Route("/historique/{id}", name="historique_edit", methods={"PUT"})
+     */
+    public function edit(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $historique = $entityManager->getRepository(Historique::class)->find($id);
+ 
+        if (!$historique) {
+            return $this->json('No historique found for id' . $id, 404);
+        }
+ 
+        $historique->setName($request->request->get('name'));
+        $historique->setDescription($request->request->get('description'));
+        $entityManager->flush();
+ 
+        $data =  [
+            'id' => $historique->getId(),
+            'name' => $historique->getNom(),
+        ];
+         
+        return $this->json($data);
     }
  
     /**
@@ -79,7 +97,7 @@ class HistoriqueController extends AbstractController
     public function delete(int $id): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
-        $historique = $entityManager->getRepository(HistoriqueDepanneur::class)->find($id);
+        $historique = $entityManager->getRepository(Historique::class)->find($id);
  
         if (!$historique) {
             return $this->json('No historique found for id' . $id, 404);
@@ -91,21 +109,5 @@ class HistoriqueController extends AbstractController
         return $this->json('Deleted a historique successfully with id ' . $id);
     }
  
-
-    // public function new(Request $request): Response
-    // {
-    //     // creates a task object and initializes some data for this example
-    //     $task = new Task();
-    //     $task->setTask('Write a blog post');
-    //     $task->setDueDate(new \DateTime('tomorrow'));
-
-    //     $form = $this->createFormBuilder($task)
-    //         ->add('task', TextType::class)
-    //         ->add('dueDate', DateType::class)
-    //         ->add('save', SubmitType::class, ['label' => 'Create Task'])
-    //         ->getForm();
-        
-    //     // ...
-    // }
  
 }
